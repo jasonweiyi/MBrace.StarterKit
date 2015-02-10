9dee3a85502b89fa5f7cd014d36533ad408a932a
krontogiannis <rodoconst@hotmail.com>
2015-02-10 17:52:32 +0200

    Initial commit : MBrace.Core 0.9.0, kmeans sample

---
+++ src/MBrace.Library/kmeans.fs
@@ -0,0 +1,81 @@
+﻿namespace MBrace.Library.KMeans
+
+open MBrace
+open System
+
+/// Represents a multi-dimensional point.
+type Point = float[]
+
+[<RequireQualifiedAccess>]
+/// K-Means algorithm implementation.
+module KMeans =
+
+    // This function partitions an array into n arrays.
+    let private partition n (a : _ array) =
+        [| for i in 0 .. n - 1 ->
+            let i, j = a.Length * i / n, a.Length * (i + 1) / n
+            Array.sub a i (j - i) |]
+
+    // This function calculates the distance between two points.
+    let private dist (arr1 : Point) (arr2 : Point) = Array.fold2 (fun acc elem1 elem2 -> acc + pown (elem1 - elem2) 2) 0.0 arr1 arr2
+
+    // This function assigns a point to the correct centroid, and returns the index of that centroid.
+    let private findCentroid (p : Point) (centroids : Point[]) : int =
+        let mutable mini = 0
+        let mutable min = Double.MaxValue
+        for i in 0..(centroids.Length - 1) do
+            let dist = dist p centroids.[i]
+            if dist < min then
+                min <- dist
+                mini <- i
+
+        mini
+
+    /// This function, given a portion of the points, calculates the number of the points assigned to each centroid,
+    /// as well as their sum.
+    let private kmeansLocal (points : Point seq) (centroids : Point[]) : (int * (int * Point))[] =
+        let lens = Array.init centroids.Length (fun _ -> 0)
+        let sums : Point[] = Array.init centroids.Length (fun _ -> Array.init centroids.[0].Length (fun _ -> 0.0 ))
+        for point in points do
+            let cent = findCentroid point centroids
+            lens.[cent] <- lens.[cent] + 1
+            for i in 0..(point.Length - 1) do
+                sums.[cent].[i] <- sums.[cent].[i] + point.[i]
+
+        Array.init centroids.Length (fun i -> (i, (lens.[i], sums.[i])))
+
+    /// The function runs the kmeans algorithm and returns the centroids of an array of Cloud Sequences of points.
+    let rec calculate (points : CloudSequence<Point>[], centroids : Point[], iteration : int) : Cloud<Point[]> =
+        let sumPoints (pointArr : seq<Point>) dim : Point =
+            pointArr
+            |> Seq.fold (fun acc elem -> let x = Array.map2 (+) acc elem in x) (Array.init dim (fun _ -> 0.0))
+
+        let divPoint (point : Point) (x : float) : Point =
+            Array.map (fun p -> p / x) point
+
+        cloud {
+            do! Cloud.Logf "KMeans iteration [%d] with centroids \n %A" iteration centroids
+            let dim = centroids.[0].Length
+
+            let! clusterParts =
+                points
+                |> Array.map (fun part -> cloud { let! points = part.ToEnumerable() in return kmeansLocal points centroids })
+                |> Cloud.Parallel
+
+            let newCent =
+                clusterParts
+                |> Seq.concat
+                |> Seq.groupBy fst
+                |> Seq.map snd
+                |> Seq.map (fun clp ->
+                    clp
+                    |> Seq.fold (fun (accN, accSum) (_, (n, sum)) -> (accN + n, sumPoints [|accSum; sum|] dim)) (0, Array.init dim (fun _ -> 0.0))
+                )
+                |> Seq.map (fun (n, sum) -> divPoint sum (float n))
+                |> Seq.toArray
+
+            if Array.forall2 (Array.forall2 (fun x y -> abs(x - y) < 1E-10)) newCent centroids then
+                return centroids
+            else
+                return! calculate(points, newCent, (iteration + 1))
+        }
---
+++ src/MBrace.Library/scripts/kmeans.fsx
@@ -0,0 +1,95 @@
+﻿
+#I "../../../lib/"
+#r "MBrace.Core.dll"
+#r "MBrace.SampleRuntime.exe"
+#r "../bin/Debug/MBrace.Library.dll"
+
+open MBrace
+open MBrace.Library.KMeans
+
+open System
+open System.Text
+open System.IO
+open System.Threading
+open System.Globalization
+open MBrace.SampleRuntime
+
+Thread.CurrentThread.CurrentCulture <- CultureInfo.InvariantCulture
+
+let folder = Path.Combine(Path.GetTempPath(), "kmeansFiles")
+let fileno = 12
+
+// The k argument of the kmeans algorithm, and the dimension of points.
+let k = 3
+let dim = 2
+
+// This function converts a sequence of 'a to a sequence of 'a arrays with dimension dim.
+let dimmed (dim : int) (inseq : seq<'a>) : seq<'a[]> =
+    let enum = inseq.GetEnumerator()
+    seq {
+        let hasNext = ref <| enum.MoveNext()
+        while !hasNext do
+            yield seq {
+                let i = ref 0
+                while !hasNext && !i < dim do
+                    yield enum.Current
+                    hasNext := enum.MoveNext()
+                    i := !i + 1
+                if !i < dim then
+                    for j = !i to dim - 1 do
+                        yield Unchecked.defaultof<'a>
+            }
+            |> Seq.toArray
+        enum.Dispose()
+    }
+
+// this function creates files with pseudo random content, in a deterministic way. (12 files =~ 512MB)
+let createFiles intArray folder =
+    for i in intArray do
+        let r = System.Random(i);
+
+        use tw = new StreamWriter(Path.Combine(folder, string i + ".txt")) 
+        [|1..64|]
+        |> Seq.map (fun _ ->
+            let b = StringBuilder()
+
+            [|1..40000|]
+            |> Seq.iter (fun _ -> b.Append(r.NextDouble() * 40.0 - 20.0).Append(" ") |> ignore)
+    
+            b.ToString()
+        )
+        |> Seq.iter tw.WriteLine
+        tw.Dispose()
+
+Directory.CreateDirectory(folder)
+createFiles [|1..fileno|] folder
+
+
+let parseFile name =
+    File.ReadLines name
+    |> Seq.collect (fun line -> line.Trim().Split(' '))
+    |> Seq.filter(fun x -> x <> "")
+    |> Seq.map (float)
+    |> dimmed dim
+
+let mkSequences =
+    [|1..fileno|]
+    |> Array.map (fun i ->
+        Path.Combine(folder, sprintf "%d.txt" i)
+            |> parseFile
+            |> CloudSequence.New
+    ) |> Cloud.Parallel
+
+MBraceRuntime.WorkerExecutable <- __SOURCE_DIRECTORY__ + "../../../../lib/MBrace.SampleRuntime.exe"
+let runtime = MBraceRuntime.InitLocal(8)
+
+let refs = runtime.RunLocal mkSequences
+
+let centroids = 
+    cloud { 
+        let! xs = refs.[0].ToEnumerable()
+        return xs |> Seq.take k |> Seq.toArray
+    } |> runtime.RunLocal
+
+
+let result = runtime.Run(KMeans.calculate(refs, centroids, 0))
